561ba4e6abd97a8e78a9b6290658f802ca749936
Louise To Thanh <louise.to.thanh@student.ehb.be>
2022-10-19 10:47:44 +0200

    opl. oef3.1-a

---
+++ scripts/utils.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import context from "./context.js";    //we gebruiken var. gebruiken die we importeren van een ander module
+
+/**
+ * Draws a line between 2 points 
+ * 
+ * @param {*} x1 x-coordinate of starting point 
+ * @param {*} x2 x-coordinate of ending point 
+ * @param {*} y1 y-coordinate of starting point 
+ * @param {*} y2 y coordinate of ending point 
+ */
+export function drawLines(x1, y1, x2, y2) {   //exp naar ander module, zonder 'default'
+    context.beginPath();
+    context.moveTo(x1,y1);
+    context.lineTo(x2,y2);
+    context.stroke();
+
+}
