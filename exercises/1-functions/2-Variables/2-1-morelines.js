b0f66fe623d458b6dae04d30cbf789b428c1459a
Louise To Thanh <louise.to.thanh@student.ehb.be>
2022-10-25 18:20:32 +0200

    oef.2.1final

--- exercises/1-functions/2-Variables/2-1-morelines.js
+++ exercises/1-functions/2-Variables/2-1-morelines.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight;
 let context = canvas.getContext('2d');
 
 let margin = 50;
-let spacing = 200;
+let spacing = 50;
 let width = context.canvas.width;
 let height= context.canvas.height;
 
@@ -15,17 +15,21 @@ drawLines();
 function drawLines(){
     context.beginPath();
     context.lineWidth = 2;
-    context.moveTo(margin,spacing);
+    context.moveTo(margin,spacing);  //Margin= wanneer je u venster kleiner maakt, blijft het 50 px (eerste aanmaken)
     context.lineTo(width-margin,spacing);
-    context.lineTo(margin, spacing);
-    context.lineTo(margin, spacing + 2);
-    context.lineTo(width-margin,spacing + 2);
-    context.lineTo(margin, spacing + 3);
-    context.lineTo(width-margin,spacing + 3);
-    context.lineTo(margin, spacing + 4);
-    context.lineTo(width-margin,spacing + 4);
-    context.lineTo(margin, spacing + 5);
-    context.lineTo(width-margin,spacing + 5);
+
+    context.lineTo(margin, spacing +100);
+   context.lineTo(width-margin,spacing + 100);
+
+    context.lineTo(margin, spacing + 200);
+    context.lineTo(width-margin,spacing + 200);
+
+    context.lineTo(margin, spacing + 300);
+    context.lineTo(width-margin,spacing + 300);
+
+    context.lineTo(margin, spacing + 400);
+    context.lineTo(width-margin,spacing + 400);
+
     context.closePath();
     context.stroke();
 
